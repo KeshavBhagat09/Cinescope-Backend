0953c3e720c36cf376842f4a3ca6225058193708
KeshavBhagat09 <sendemailtokeshav@gmail.com>
2025-02-24 23:00:49 +0530

    utils code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(statusCode, 
+      message="Sucess",
+      data
+  ) {
+      this.status = statusCode;
+      this.message = message;
+      this.data = data;
+      this.success = statusCode < 400;
+    }
+  }
+  export { ApiResponse }
