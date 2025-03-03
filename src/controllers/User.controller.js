26de3fd56c24b59605d404fa8139ee05e4c8caed
KeshavBhagat09 <sendemailtokeshav@gmail.com>
2025-03-03 19:16:40 +0530

    fix: update user registration logic and improve file handling

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -2,28 +2,37 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {uploadOnCloudinary} from "../utils/cloudyinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import {User} from "../models/User.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "ok"
     })
-    const{fullName, email, username, password} = req.body
-    console.log("email", email);
+    const{fullname, email, username, password} = req.body
+   //  console.log("email", email);
 
    if(
-    [fullName, email, username, password].some((field) =>
+    [fullname, email, username, password].some((field) =>
     field?.trim() === "")
    ) {
         throw new ApiError(400, "All fields are required")
      }    
-     const existedUser = await user.findOne({
+     const existedUser = await User.findOne({
         $or: [{ username },{ email }]
      })
+
      if(existedUser){
          throw new ApiError(409, "User with email or username already exists")
      }
-     req.files?.avatar[0]?.path;
-     const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+     const avatarLocalPath = req.files?.avatar[0]?.path;
+   //   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+
+   let coverImageLocalPath;
+   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path;
+   }
 
      if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required")
@@ -36,15 +45,15 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Avatar is required")
      }
 
-     const user = await user.create({
-        fullName,
+     const Userr = await User.create({
+        fullname,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
         username:username.toLowerCase()
      })
-     const createdUser = await user.findById(user._id).select(
+     const createdUser = await User.findById(User._id).select(
         "-password -refreshToken"
      )
 
