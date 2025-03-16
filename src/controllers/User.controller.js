9edd41c2668e2f18295ab644a9e18500458405ad
KeshavBhagat09 <sendemailtokeshav@gmail.com>
2025-03-16 22:55:28 +0530

    add login functionality with token generation and user validation

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -75,7 +75,7 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage?.url || "",
     email,
     password,
-    Username: Username.toLowerCase(),
+    Username: Username.toLowerCase(), 
   });
   console.log("user", user);
 
@@ -92,6 +92,75 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> data
+  // username or email
+  //find the user
+  //password check
+  //access and referesh token
+  //send cookie
+
+  const { email, Username, password } = req.body;
+  console.log(email);
+
+  if (!Username && !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  // Here is an alternative of above code based on logic discussed in video:
+  // if (!(username || email)) {
+  //     throw new ApiError(400, "username or email is required")
+
+  // }
+
+  
+
+  const user = await User.findOne({
+    $or: [{ Username }, { email }],
+  }).select("+password"); // ✅ Ensure password is retrieved
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+
 export{
    registerUser,
+   loginUser,
 }
